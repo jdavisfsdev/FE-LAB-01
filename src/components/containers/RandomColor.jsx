0aa78553d524f3986fa9aafe8c7988580062ea89
Joseph Davis <joethebartenderpdx@gmail.com>
2021-07-26 16:51:10 -0700

    color changes every second

--- src/components/containers/RandomColor.jsx
+++ src/components/containers/RandomColor.jsx
@@ -1,20 +1,27 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
 import ColorDisplay from '../ColorDisplay';
 import { colorGenerator } from '../services/ColorGenerator';
 
 export default class RandomColor extends Component {
     state = {
-      randomColor: 'cyan'
+      randomColor: ''
     }
 
     handleColorChange = () => {
-      const colors = ['green', 'yellow', 'cyan', 'pink'];
+      const colors = ['green', 'yellow', 'cyan', 'pink', 'black', 'blue', 'red'];
       const color = colorGenerator(colors);
       this.setState({ randomColor: colors[color] });
     }
     componentDidMount() {
-      this.handleColorChange();
+      this.interval = setInterval(() => this.handleColorChange(), 1000);
     }
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+    // componentDidUpdate() {
+    //   setInterval(this.handleColorChange(), 1000);
+    // }
 
     render() {
       return (
